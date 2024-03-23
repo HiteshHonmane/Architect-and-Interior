bbab4e020ee835723b6d8d26d472e7ff6b757e39
HiteshHonmane <hiteshhonmane@gmail.com>
2024-03-23 15:10:19 +0530

    feat : Smooth scroll
    Style: Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 import Home from './components/home/Home'
 import './App.css'
 import Contact from './components/contact form/Contact'
+import Lenis from '@studio-freight/lenis'
+import Footer from './components/footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+ // smooth Scroll function
+const lenis = new Lenis()
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
   return (
     <>
     <Home/>
     <Contact/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import sketch from '../../assets/images/sketch.svg'
+
+function Footer() {
+  return (
+    
+    <>
+
+    <div className='w-screen relative h-screen bg-[#141414]   '>
+        <img className= ' opacity-50 w-full h-[80vh] invert object-cover object-bottom scale-150 ' src={sketch} alt="" />
+    </div>
+    <div className=''>
+    
+      
+    </div>
+
+    
+    
+    </>
+      
+
+    
+
+    
+    
+    
+  )
+}
+
+export default Footer

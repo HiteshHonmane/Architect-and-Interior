e6797b927afb1b4582b0c49c01bb7f4c3f91274c
HiteshHonmane <hiteshhonmane@gmail.com>
2024-04-01 14:20:09 +0530

    Created : Blog card and page

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import DateSvg from '../../assets/icons/date.svg'
+import WritingSvg from '../../assets/icons/writing.svg'
 
-function blog() {
+function blog(props) {
   return (
-    <div>blog</div>
+    <div className=' sm:w-[594px] sm:h-[543px] '>
+      <div className=' w-full h-[435px] flex flex-col object-contain overflow-hidden '>
+        <img className='w-full h-[435px]' src={props.imglink} alt="" /> 
+      </div>
+      
+      <div className=' flex flex-wrap w-full mt-8   '>
+      <div className=' font-satoshi mx-3  py-1 px-2 border rounded-lg font-medium text-xs  uppercase  '>
+        {props.tag}
+      </div>
+
+      <div className=' font-satoshi font-medium text-  w-full text-[20px] px-3 uppercase '>
+        {props.title}
+      </div>
+
+      <div className=' flex gap-1 flex-row justify-between items-center w-full p-3'>
+        <img width={18} src={WritingSvg} alt="" />
+        <span className='  w-[30%]  text-xs text-[#A6A6A4] uppercase '>
+          {props.writer}
+        </span>
+        <div className='bg-slate-400 h-[1px] w-full  '>
+
+        </div>
+
+        <div className='w-[48%] flex justify-center gap-1 '>
+          <img width={18} src={DateSvg} alt="" />
+          <span className=' font-satoshi text-xs uppercase   text-[#A6A6A4]'>
+            {props.date}
+          </span>
+        </div>
+
+      </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -2,11 +2,38 @@
 import React from 'react'
 import blogsvg from '../assets/icons/blog.svg'
 import Button from '../components/button/Button'
+import BlogCard from '../components/blog/BlogCard'
 
 function BlogPage() {
+
+  const blog1= {
+    tag:'design inspirations',
+    title: 'Transforming spaces with timeless design',
+    writer: 'james miller',
+    date:'january 18, 2024',
+    imglink:'https://framerusercontent.com/images/Jm14NVS1DWCTSNRjQLkBTLtEKok.jpg?scale-down-to=1024',
+    clicklink:''
+  }
+  const blog2= {
+    tag:'design insight',
+    title: 'the social impacct of architectural innovation',
+    writer: 'patrick watson',
+    date:'january 11, 2024',
+    imglink:'https://framerusercontent.com/images/MDtFSJaF3MhabVjn223xx2IMk.jpg?scale-down-to=1024',
+  }
+  const blog3= {
+    tag:'interior design insights',
+    title: 'the impact of interior on wellbeing',
+    writer: 'rayan milford',
+    date:'december 5, 2023',
+    imglink:'https://framerusercontent.com/images/v1ltoVHr1qjuj63noI3YsrEqSNg.jpg?scale-down-to=1024',
+  }
+
+
+
   return (
     <div className="px-12  overflow-clip w-screen h-screen bg-[#F8F8F5] ">
-    <div className="flex flex-col w-full h-[40vh] ">
+    <div className="flex flex-col w-full h-[30vh] ">
       <div className=" text-xs flex flex-row font-satoshi">
         <img width={15} src={blogsvg} alt="" />
         BLOGS & INSIGHTS
@@ -16,19 +43,20 @@ function BlogPage() {
       </div>
       <div className="flex relative px-12 py-6 justify-end">
         <p className=" font-satoshi text-[18px]  ">
-          We make even the wildest architectural dreams into <br /> dramatic
-          yet feasible finished products anyone <br />
-          would be proud of.
+        Stay on the pulse of the latest architecture trends <br /> and thinking via our collection of insights.
         </p>
       </div>
       <div className="w-full h-[2px] bg-[#141414]"></div>
       <div className="w-full flex mt-5 justify-end  ">
         <div className="">
-          {" "}
           <Button name={"Explore Services"} />{" "}
         </div>
-        
       </div>
+    </div>
+    <div className=' flex flex-row gap-3'>
+          <BlogCard {...blog1}/>
+          <BlogCard {...blog2}/>
+          <BlogCard {...blog3}/>
     </div>
     
     <div className=" flex gap-3"></div>

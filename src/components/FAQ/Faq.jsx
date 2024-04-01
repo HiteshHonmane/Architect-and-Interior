b57653786792d8dccb519d5658de44b9e790dd77
HiteshHonmane <hiteshhonmane@gmail.com>
2024-04-01 14:20:59 +0530

    Feat :  unhide hide answers

--- src/components/FAQ/Faq.jsx
+++ src/components/FAQ/Faq.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import plusSvg from '../../assets/icons/plus.svg'
-function Faq() {
-  return (
-    <div className=' w-full  mt-12  border-b   '>
-       <div className='flex px-11 h-16 gap-20 item-center align-middle hover:bg-slate-300  '  >
-        <img src={plusSvg} width={14} alt="" />
-        <div className=' flex font-satoshi font-medium text-base items-center text-[#20201F]' > WHAT ARCHITECTURAL DESIGN SERVICES DO YOU OFFER? </div>
-       </div>
-    </div>
-  )
+import React, { useState } from "react";
+import plusSvg from '../../assets/icons/plus.svg';
+
+function Faq(props) {
+    const [isExpanded, setExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setExpanded(!isExpanded);
+    };
+
+    return (
+        <div className="w-full mt-6 border-b flex flex-col cursor-pointer  ">
+            <div className="flex px-11 h-16 gap-20 items-center align-middle hover:bg-[#EDEDEA] hover:transition-all duration-500 ease-out  " onClick={toggleExpand}>
+                <img src={plusSvg} width={14} alt="" />
+                <div className="flex font-satoshi font-medium text-base text-[#20201F] w-[100%] ">
+                  {props.question}
+                </div>
+            </div>
+            <div className={`faq-answer p-2 font-satoshi text-base font-normal transition-all duration-500 ease-in-out ml-10 ${isExpanded ? 'block' : 'hidden'}`}>
+                {props.answer}
+            </div>
+            
+        </div>
+    );
 }
 
-export default Faq
+export default Faq;

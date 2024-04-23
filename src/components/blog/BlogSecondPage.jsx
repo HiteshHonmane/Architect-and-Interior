5e2711a96a0ad30b39c37a8c04a81a3c171b9a16
HiteshHonmane <hiteshhonmane@gmail.com>
2024-04-23 23:03:45 +0530

    refactor

--- src/components/blog/BlogSecondPage.jsx
+++ src/components/blog/BlogSecondPage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function BlogSecondPage() {
+  const font = {
+    font: " font-satoshi ",
+  };
+
   return (
     <div className=" w-full h-screen">
       <div className=" flex relative">
@@ -9,11 +13,15 @@ function BlogSecondPage() {
           src="https://framerusercontent.com/images/Jm14NVS1DWCTSNRjQLkBTLtEKok.jpg?scale-down-to=1024"
           alt=""
         />
-        <div className=" absolute h-[50%]  w-full bg-black flex flex-col  justify-center  ">
-          <h1 className=" relative bg-black text-white text-3xl h-[20%] w-[50%] text-center flex items-start ">
-            THE SOCIAL IMPACT OF ARCHITECTURAL INNOVATION
+        <div className=" absolute h-[50%] bg-gradient-to-b from-[#000] to-[#0000007c]  w-[100vw]bg-gradient-to-b  flex flex-col  justify-center  items-center ">
+          <h1
+            className={`" relative bg-black text-white text-[68px] h-[30%] w-[40%] text-center flex items-start font-medium  ${font.font} "`}
+          >
+            THE SOCIAL IMPACT OF <br /> ARCHITECTURAL <br /> INNOVATION
           </h1>
-          <p className=" text-white w-[50%]  ">
+          <p
+            className={`" text-white w-[35%] ${font.font} text-[24px]  text-gray-300  text-center  "`}
+          >
             Verdant believes that architecture goes beyond creating physical
             spaces; it has the power to be a social catalyst. Our projects are
             designed with a purpose, aiming to foster community engagement,
